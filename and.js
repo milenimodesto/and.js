62bcd9487d0d835f39157e331ba5ccd5faa2d2e4
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-04-04 11:47:40 -0300

    Add files via upload

---
+++ and.js
@@ -0,0 +1,7 @@
+let idade = 25;
+let possuiCarteiaDeMotorista = true;
+if (idade >= 18 && possuiCarteiaDeMotorista) {
+    console.log( "voçẽ pode dirigir.");
+}else {
+     console.log("voçê não pode diriir");
+}
