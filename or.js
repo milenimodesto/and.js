d2ca80f44aa6e52ad2564f26fc8e8e8a48e78ec8
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-04-04 11:53:07 -0300

    Add files via upload

---
+++ or.js
@@ -0,0 +1,6 @@
+let idade = 25;
+if (idade < 18 || idade >65){
+    console.log("voçê não é legível para o desconto de idoso.");
+}else{
+    console.log("você não é eleíve para o deconto de doce");
+}
